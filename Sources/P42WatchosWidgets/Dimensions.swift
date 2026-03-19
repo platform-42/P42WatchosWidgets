2a8b9ab074302e905a119e2b00b2956712b1a67d
platform-42 <142492114+platform-42@users.noreply.github.com>
2026-03-19 14:18:09 +0100

    ddddd

--- Sources/P42WatchosWidgets/Dimensions.swift
+++ Sources/P42WatchosWidgets/Dimensions.swift
@@ -4,7 +4,9 @@
 //
 //  Created by Diederick de Buck on 19/03/2026.
 //
+
 import Foundation
+
 
 enum HeaderDimension {
     static let fontSize: CGFloat = 14
@@ -13,12 +15,14 @@ enum HeaderDimension {
     static let vSpacing: CGFloat = 2
 }
 
+
 enum FooterDimension {
     static let fontSize: CGFloat = 10
     static let cornerRadius: CGFloat = 4
     static let hSpacing: CGFloat = 6
     static let vSpacing: CGFloat = 4
 }
+
 
 enum MetricsDimension {
     static let iconSize: CGFloat = 28
@@ -27,6 +31,7 @@ enum MetricsDimension {
     static let labelFontSize: CGFloat = 12
     static let cornerRadiusRow: CGFloat = 12
 }
+
 
 enum FunnelDimension {
     static let iconSize: CGFloat = 28
