ad37ad718caa14ba9e134b866d7c33d0ceda9fc9
platform-42 <142492114+platform-42@users.noreply.github.com>
2026-03-29 11:41:52 +0200

    dddd

--- Sources/P42WatchosWidgets/Views/Shops42/HistoryListView.swift
+++ Sources/P42WatchosWidgets/Views/Shops42/HistoryListView.swift
@@ -36,18 +36,32 @@ public struct HistoryListItem: Identifiable {
 }
 
 public struct HistoryListView: View {
+    public let title: String
+    public let titleBadge: String?
     public let historyList: [HistoryListItem]
-    
+    public let latency: String?
+
     public init(
-        historyList: [HistoryListItem]
+        title: String,
+        titleBadge: String? = nil,
+        historyList: [HistoryListItem],
+        latency: String? = nil
     ) {
+        self.title = title
+        self.titleBadge = titleBadge
         self.historyList = historyList
+        self.latency = latency
     }
     
     public var body: some View {
         
         VStack {
             List{
+                headerView(
+                    title: title,
+                    titleBadge: "",
+                    latency: latency
+                )
                 ForEach(historyList) { order in
                     HStack {
                         VStack {
@@ -76,3 +90,44 @@ public struct HistoryListView: View {
 }
 
 
+extension HistoryListView {
+    
+    @ViewBuilder
+    private func headerView(
+        title: String,
+        titleBadge: String?,
+        latency: String?
+    ) -> some View {
+        VStack(alignment: .leading, spacing: 4) {
+            HStack {
+                if let badge = titleBadge {
+                    Image(systemName: badge)
+                        .font(.system(size: KPIDimension.badgeFontSize, weight: .semibold))
+                        .foregroundStyle(.white)
+                        .padding(KPIDimension.badgeInnerPadding)
+                        .background(Color(hex: ColorRGB.badge))
+                        .clipShape(Circle())
+                }
+                Text(title)
+                    .font(.system(
+                        size: HeaderDimension.fontSize,
+                        weight: .medium,
+                        design: .rounded
+                    ))
+                    .foregroundColor(Color(hex: ColorRGB.title))
+                if let latency = latency {
+                    Rectangle()
+                        .fill(Color(hex: ColorRGB.footerBackground))
+                        .frame(width: 1, height: HeaderDimension.fontSize)
+                    Text(latency)
+                        .font(.system(size: HeaderDimension.fontSize, weight: .light))
+                        .foregroundStyle(Color(hex: ColorRGB.latency))
+                        .monospacedDigit()
+                }
+                Spacer()
+            }
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .padding(.horizontal, HeaderDimension.hSpacing)
+    }
+}
